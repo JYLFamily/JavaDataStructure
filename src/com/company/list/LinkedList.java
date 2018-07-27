73a0fcfcbeec22d6d7291df840eae83bf844ea6a
JYLFamily <1579502635@qq.com>
2018-07-27 19:49:37 +0800

    queue

--- src/com/company/list/LinkedList.java
+++ src/com/company/list/LinkedList.java
@@ -9,4 +9,44 @@ public class LinkedList<E> {
         this.head = null;
         this.size = 0;
     }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void addFirst(E element) {
+        Node<E> node = new Node<>(element, this.head);
+        this.head = node;
+        this.size = this.size + 1;
+    }
+
+    public void add(int index, E element) {
+        // index = size 相当于在链表末尾插入元素
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Index Error.");
+
+        if (index == 0) {
+            addFirst(element);
+        } else {
+            Node<E> prev = this.head;
+            Node<E> node = new Node<>(element, null);
+
+            // 迭代到使得 prev 指向插入 element 之前那个 element
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
+        }
+
+        this.size = this.size + 1;
+    }
+
+    public void addLast(E element) {
+        this.add(this.size, element);
+    }
 }
